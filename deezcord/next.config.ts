4fede0e71893cb944d244dce4fc8f3f6039f06f7
HyuseCS <christianjohn.legaspi@1.ustp.edu.ph>
2026-04-24 23:59:01 +0800

    refactor: Migrated from NextJS framework to Vite + React

--- deezcord/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['localhost', '192.168.1.104'],
-};
-
-export default nextConfig;
